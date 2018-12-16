30f21925fbfc637aa9310529dff29c6f4e3e7699
Nick Morgan <nick@nicholas-morgan.com>
2018-12-16 07:32:31 -0800

    Changed homepage verbiage.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -13,7 +13,7 @@
 
             <p>I am based in Los Angeles, CA. I work in <a class="hover-up" href="https://laravel.com" target="_blank">Laravel - The PHP Framework for Web Artisans</a> because I like things that are good, and use a test-driven (TDD) development approach.</p>
 
-            <p>My experience covers Server Configuration, Database Administration, HTML5, Security, git Version Control System, Agile/Scrum, Business Development, Search Engine Optimization, and probably more.</p>
+            <p>My experience covers Server Configuration, Database Administration, HTML5, Security, git Version Control System, Agile/Scrum, Business Development, Search Engine Optimization, OAuth2 implementation, and probably more.</p>
 
             <p><a class="hover-up" href="https://github.com/skcin7" target="_blank">Github</a></p>
         </div>
