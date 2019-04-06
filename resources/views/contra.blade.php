dd18a39aff322b72b68d2ff3fe55adea7cfdfb9c
Nick Morgan <nick@nicholas-morgan.com>
2019-04-06 09:33:56 -0700

    Contra fixes.

--- resources/views/contra.blade.php
+++ resources/views/contra.blade.php
@@ -15,7 +15,7 @@
     <a class="menu-item" href="#" data-toggle="modal" data-target="#modal-controls">Controls</a>
 </header>
 <div class="container" id="container">
-    <canvas id="nes-canvas" width="256" height="240" />
+    <canvas id="nes-canvas" width="256" height="240"/>
 </div>
 
 <div class="modal my-modal fade" id="modal-controls" tabindex="-1">
@@ -27,12 +27,14 @@
             </div>
             <div class="modal-body">
 
+                <p class="text-uppercase">Will you destroy the Vile Red Falcon and save the universe?</p>
+
                 <table class="table table-bordered table-hover table-nonfluid table-sm table-responsive-sm">
                     <thead class="thead-dark">
                         <tr>
                             <th>Button</th>
                             <th>Player 1</th>
-                            <th>Player 2 <span style="font-weight: normal;">(Coming Soon)</span></th>
+                            <th>Player 2 <span class="font-weight-normal">(Soon)</span></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -79,8 +81,8 @@
                     </tbody>
                 </table>
 
-                <p class="mb-0 text-muted">Note: Sound does not work in Safari, but appears to work in all other major browsers. Make sure to click inside the game canvas area to ensure the browser is focused on the game so that your input keys will be recognized. Will you destroy the Vile Red Falcon and save the universe?</p>
-                <p class="mb-0 text-muted"><small><strong>Thanks and credit to <a href="https://github.com/bfirsh/jsnes" target="_blank">JSNES</a> for the extremely awesome emulator!</strong></small></p>
+                <p class="mb-0 text-muted">NOTE: Sound does not work in Safari, but appears to work in other major browsers. Make sure to click inside the canvas area to ensure the browser is focused on the game so that your input keys will be recognized.</p>
+                <p class="mb-0 text-muted"><small><strong>Thanks to <a href="https://github.com/bfirsh/jsnes" target="_blank">JSNES</a> for the awesome NES emulator!</strong></small></p>
             </div>
             <div class="modal-footer">
                 <button class="btn btn-primary" type="button" data-dismiss="modal">Okay Whatever</button>
