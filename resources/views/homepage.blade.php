55f2b38873e4de5f49b6d2826daf36bd3fe807fc
Nick Morgan <nick@nicholas-morgan.com>
2019-04-06 09:37:43 -0700

    Contra fixes.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,13 +9,11 @@
         @include('_errors')
 
         <div class="about-container">
-            <p class="mt-3">I'm a Full Stack Web, API developer, and Video Game Historian with over 6 years of industry experience in crafting highly usable fully scalable interactive web applications.</p>
+            <p class="mt-3">I'm a Full Stack Web and API developer with over 8 years of industry experience in crafting highly usable fully scalable interactive web applications.</p>
 
-            <p>I am based in Los Angeles, CA. I work in <a class="hover-up" href="https://laravel.com" target="_blank">Laravel - The PHP Framework for Web Artisans</a> because I like things that are good, and use a test-driven (TDD) development approach to ensure things don't break. I'm fluent in all aspects of the software development stack, including .</p>
+            <p>I am based in Los Angeles, CA. I work in <a class="hover-up" href="https://laravel.com" target="_blank">Laravel - The PHP Framework for Web Artisans</a> because I like things that are good, and use a test-driven (TDD) development approach. My experience covers server configuration, database administration, HTML5 and front-end development (including SCSS and various JavaScript frameworks), SSH key configuration and basic security, Git version control system, working in team based agile/scrum environments, business development, search engine optimization, OAuth2 implementation, API development and implementation, and probably more.</p>
 
-            {{--<p>My experience covers server configuration, database administration, HTML5 and front-end development (including SCSS and various JavaScript frameworks), SSH key configuration and basic security, Git version control system, working in team based agile/scrum environments, business development, search engine optimization, OAuth2 implementation, API development and implementation, and probably more.</p>--}}
-
-            {{--<p>I use Bitbucket now (free private repositories!) but here's my public <a class="hover-up" href="https://github.com/skcin7" target="_blank">Github</a>.</p>--}}
+            <p>I use Bitbucket now (free private repositories!) but here's my public <a class="hover-up" href="https://github.com/skcin7" target="_blank">Github</a>.</p>
         </div>
 
     </div>
