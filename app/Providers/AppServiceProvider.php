c7b5f65fb8bd63aed7fe46a025458ba02101c74b
Nick Morgan <nick@nicholas-morgan.com>
2019-10-10 13:45:22 -0700

    Deploy script used to push to production server on 10/10/2019_13:45:11.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // IP addresses to be whitelisted to show debugging for.
+        if(in_array(get_ip_address(), [
+            '127.0.0.1',
+            '73.66.215.230', // Nick
+            '96.71.229.83', // Nick Starbucks
+        ])) {
+            config(['app.debug' => true]);
+        }
     }
 }
