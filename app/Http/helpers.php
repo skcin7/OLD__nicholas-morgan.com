92a1328d3d96c5334207c2c8a84eb91accc8e02c
Nick Morgan <nick@nicholas-morgan.com>
2019-10-10 13:46:08 -0700

    Deploy script used to push to production server on 10/10/2019_13:45:57.

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -19,6 +19,30 @@ function admin() {
 }
 
 /**
+ * Get IP address.
+ *
+ * @return string
+ */
+function get_ip_address() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+/**
  * Slugify some text.
  *
  * @param type $str
