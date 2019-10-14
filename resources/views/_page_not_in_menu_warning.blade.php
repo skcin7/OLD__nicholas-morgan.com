bd6e426252e6c1dc869e6fec986b63ee664d282c
Nick Morgan <nick@nicholas-morgan.com>
2019-10-14 08:53:20 -0700

    Deploy script used to push to production server on 14/10/2019_08:53:10.

--- resources/views/_page_not_in_menu_warning.blade.php
+++ resources/views/_page_not_in_menu_warning.blade.php
@@ -1,3 +1,3 @@
 @if(! Setting::get('show_' . $page) && admin())
-    <div class="alert alert-warning">NOTE: The link to this page ({{ $page }}') is not showing publicly, but is showing to you, because you're an admin.</div>
+    <div class="alert alert-warning">NOTE: The link to this page ({{ $page }}') is not showing in the menu, but is showing to you, because you're an admin.</div>
 @endif
