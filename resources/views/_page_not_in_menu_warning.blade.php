369748b0c33688fa8102fcb28508b7ee05282009
Nick Morgan <nick@nicholas-morgan.com>
2019-10-14 10:25:32 -0700

    Deploy script used to push to production server on 14/10/2019_10:25:23.

--- resources/views/_page_not_in_menu_warning.blade.php
+++ resources/views/_page_not_in_menu_warning.blade.php
@@ -1,3 +1,3 @@
 @if(! Setting::get('show_' . $page) && admin())
-    <div class="alert alert-warning">NOTE: This page is not showing in the menubar.</div>
+    <div class="alert alert-warning">NOTE: The link for this page is not showing in the menubar.</div>
 @endif
